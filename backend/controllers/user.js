bc68101c26ba7526128aff58c696dd82dc7a4de7
Hassan198646 <hasan.niazi333@gmail.com>
2024-09-13 16:48:20 +0500

    working on password hashing

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,18 +1,20 @@
-const user = require('../models/user')
-module.exports = function(app){
+const user = require("../models/user");
+const hashedPassword = require("bcryptjs");
+module.exports = function (app) {
   const { createUser } = user;
   const user_base_url = "/api/user";
-  
-  app.post(`${user_base_url}`, async(req,res)=>{
-    try{
-        const response = await createUser(req.body);
-        console.log(response)
-        res.status(200).json(response);
+
+  app.post(`${user_base_url}`, async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const userPassword = await hashedPassword.hash(password, 10);
+      const newUser = { username, email, password: userPassword };
+      const response = await createUser(newUser);
+      console.log(response);
+      res.status(200).json(response);
     } catch (err) {
       console.error(err);
       res.status(500).json({ status: false, error: "Internal Server Error" });
     }
-  })
-} 
-
-
+  });
+};
