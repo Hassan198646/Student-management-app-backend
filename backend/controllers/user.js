8a5e7562a0669e8f9c0da46c16b26a7f5377ffe6
Hassan198646 <hasan.niazi333@gmail.com>
2024-09-17 16:20:13 +0500

    working on auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const user = require("../models/user");
 const hashedPassword = require("bcryptjs");
 module.exports = function (app) {
-  const { createUser } = user;
+  const { createUser, loginUser } = user;
   const user_base_url = "/api/user";
 
   app.post(`${user_base_url}`, async (req, res) => {
@@ -10,6 +10,18 @@ module.exports = function (app) {
       const userPassword = await hashedPassword.hash(password, 10);
       const newUser = { username, email, password: userPassword };
       const response = await createUser(newUser);
+      console.log(response);
+      res.status(200).json(response);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ status: false, error: "Internal Server Error" });
+    }
+  });
+
+  app.post(`${user_base_url}/login`, async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const response = await loginUser(email, password);
       console.log(response);
       res.status(200).json(response);
     } catch (err) {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,18 +1,36 @@
-const knex = require ('../knex');
-  async function createUser(args) {
-    try {
-      const response = await knex("user").insert({
-        id: args.id,
-        username: args.username,
-        email: args.email,
-        password: args.password,
-      });
-      return { status: true, response: response };
-    } catch (err) {
-      return { status: false, error: err };
-    }
+const knex = require("../knex");
+async function createUser(args) {
+  try {
+    const response = await knex("user").insert({
+      id: args.id,
+      username: args.username,
+      email: args.email,
+      password: args.password,
+    });
+    return { status: true, response: response };
+  } catch (err) {
+    return { status: false, error: err };
   }
-  module.exports = {
-    createUser,
-  };
+}
 
+async function loginUser(email) {
+  try {
+    const response = await knex
+      .select("email")
+      .from("user")
+      .where("email", email);
+    if (response.length > 0) {
+      return { status: true, user: response[0] }; 
+    } else {
+      return { status: false, message: "User not found" }; 
+    }
+  } catch (err) {
+    return { status: false, error: err };
+  }
+}
+
+
+module.exports = {
+  createUser,
+  loginUser
+};
