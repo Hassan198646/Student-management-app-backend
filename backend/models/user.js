b0e3ccd1a8dad2c5ee0ba82f7efb40fb8110f2dc
Hassan198646 <hasan.niazi333@gmail.com>
2024-09-20 15:19:41 +0500

    working on auth

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,8 @@
 const knex = require("../knex");
 const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY =
+  "3f7d0a9d82eaf7479e4a1e2cfe317be55c25bcd67a1f97da53813a9bc1c7f2f9";
 async function createUser(args) {
   try {
     const response = await knex("user").insert({
@@ -25,11 +28,18 @@ async function loginUser(email, password) {
       return { staus: false, user: "User Not Found" };
     }
     const isPasswordCorrect = await bycrypt.compare(password, user.password);
-    if (isPasswordCorrect) {
-      return { status: true, message: "Login Successfuly" };
-    } else {
-      return { status: false, message: "Invalid Password" };
+    if (!isPasswordCorrect) {
+      return { status: false, message: "Invalid password" };
     }
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        email: user.email,
+      },
+      SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    return { status: true, token: token, message: "Login successful" };
   } catch (err) {
     return { status: false, error: err };
   }
